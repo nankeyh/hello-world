e56e1d89f2259aeb4c32a41b00624ba31d618d30
nankeyh <huyangbeigui@163.com>
2024-03-23 20:51:20 +0800

    C++

---
+++ srccode/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+	cout<<"hello world !"<<endl;
+
+	return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
